1b1b54400505f7d7a13fb9c99a56faf7e968ee11
gabrielcubidesmt <gabriel.cubides.mt@correo.usa.edu.co>
2021-12-19 23:14:14 -0500

    fixed

--- src/main/java/usa/ciclo4/app/repositories/crud/ClothesCrudRpository.java
+++ src/main/java/usa/ciclo4/app/repositories/crud/ClothesCrudRpository.java
@@ -2,10 +2,12 @@ package usa.ciclo4.app.repositories.crud;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Component;
 import usa.ciclo4.app.entities.Clothes;
 
 import java.util.List;
 
+@Component
 public interface ClothesCrudRpository extends MongoRepository<Clothes, String> {
     public List<Clothes> findByPriceLessThanEqual(double price);
 
